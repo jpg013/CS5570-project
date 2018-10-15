4736d126d0bb15e40dba99f19f5622d83bb2f1e8
Justin Graber <jpg013@gmail.com>
2018-10-14 20:59:55 -0500

    Added ACA to recoverability engine.

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from token_processor import create_transactions_from_input
 from recoverable_engine import RecoverableEngine
 
 def main():
-    input_str = "w1(x) r3(z) r2(x) c2 c1"
+    input_str = "w1(x) r2(x) c1 c2"
 
     token_result_set = create_transactions_from_input(input_str)
     hist = History()
     hist.set_transactions(token_result_set.transactions)
     hist.set_schedule(token_result_set.data_operations)
     hist.pretty_print()
+    
     r_engine = RecoverableEngine(hist)
-    r_engine.run()
-    r_engine.pretty_print()
+    recoverable_result = r_engine.is_recoverable()
+    
+    print(recoverable_result.message)
 
+    aca_result = r_engine.is_aca()
+    print(aca_result.message)
+    
 if __name__== "__main__":
     main()
--- recoverable_engine.py
+++ recoverable_engine.py
@@ -3,6 +3,69 @@ import collections
 
 read_set_result = collections.namedtuple('ReadSetResult', ['read_op', 'write_op'])
 
+class IsRecoverableResult:
+    def __init__(self, violations):
+        self.violations = violations
+        self.is_recoverable = len(self.violations) == 0
+        self.message = self.format_message()
+
+    def format_message(self):
+        if self.is_recoverable:
+            return 'history is recoverable.'
+
+        base_message = 'history is not recoverable because '
+
+        for idx, v in enumerate(self.violations):
+            read_tx = v[0]
+            write_tx = v[1]
+            
+            read_formatted_commit_type = read_tx.commit_type().name.lower() + 's'
+            write_formatted_commit_type = write_tx.commit_type().name.lower() + 's'
+
+            msg = ''
+
+            if idx > 0:
+                msg = msg + ' and '
+
+            msg = 'T{0} reads from T{1} and T{0} {2} before T{1} {3}'.format(read_tx.transaction_id, write_tx.transaction_id, read_formatted_commit_type, write_formatted_commit_type)
+            
+            base_message = base_message + msg
+
+            if idx >= (len(self.violations) - 1):
+                base_message = base_message + '.'
+
+        return base_message
+
+class ACAResult:
+    def __init__(self, violations):
+        self.violations = violations
+        self.is_aca = len(self.violations) == 0
+        self.message = self.format_message()
+
+    def format_message(self):
+        if self.is_aca:
+            return 'history is aca.'
+
+        base_message = 'history is not aca because '
+
+        for idx, v in enumerate(self.violations):
+            read_tx = v[0]
+            write_tx = v[1]
+            
+            msg = ''
+
+            if idx > 0:
+                msg = msg + ' and '
+
+            msg = 'T{0} reads from T{1} before T{1} commits it data operations'.format(read_tx.transaction_id, write_tx.transaction_id)
+            
+            base_message = base_message + msg
+
+            if idx >= (len(self.violations) - 1):
+                base_message = base_message + '.'
+
+        return base_message
+
 class RecoverableEngine:
     """RecoverableEngine class. Given a history will determine whether or not
     the history is recoverable, avoids cascading aborts (aca), and is strict."""
@@ -12,6 +75,7 @@ class RecoverableEngine:
             raise ValueError("History must be defined.")
       
         self.history = history
+        # set of read/write tuples where read op reads data from write op for data item
         self.read_set = set()
 
         # a list containing the order that each transaction in the history either commits/aborts. List items
@@ -19,34 +83,61 @@ class RecoverableEngine:
         # before T2 commits/aborts
         self.tx_commit_abort_order = []
 
-        # set containing read_set recoverability infractions, read/write pairs that are not recoverable.
-        self.recoverability_infractions = set()
-  
-    def run(self):
-        #self.build_node_edges()
         self.construct_read_set()
-        self.determine_transaction_terminate_order()
-        self.determine_recoverability()
-        print(len(self.recoverability_infractions))
+        self.build_transaction_commit_abort_order()
 
-    def determine_recoverability(self):
+
+    def is_aca(self):
+        """To determine aca, if T1 reads from T2, the T2 must commit before any operation in T1 reads data that is 
+        written by T2. Returns a tuple where first value is a bool indicating recoverability, the 
+        second value is the set of recoverable violations. It will be empty if history is recoverable."""
+
+        violations = set()
+        schedule = self.history.schedule[0:]
+
+        for item in self.read_set:
+            read_op  = item.read_op
+            write_op = item.write_op
+
+            read_tx = self.history.get_transaction_by_id(read_op.transaction_id)
+            write_tx = self.history.get_transaction_by_id(write_op.transaction_id)
+
+            start_idx = schedule.index(write_op)
+            end_idx = schedule.index(read_op)
+            new_slice = schedule[start_idx: end_idx]
+
+            commit_found = any(x.is_commit() and x.transaction_id == write_op.transaction_id for x in schedule[start_idx:end_idx])
+            
+            # check if violates aca
+            if not commit_found:
+                violations.add((read_tx, write_tx))
+
+        return ACAResult(violations)
+        
+    def is_recoverable(self):
         """To determine recoverability, for each graph node edge T1 -> T2, where T1 read from T2,
-        c2 < c1 or a1 < a2."""
+        c2 < c1 or a1 < a2. Returns a tuple where first value is a bool indicating recoverability, the 
+        second value is the set of recoverable violations. It will be empty if history is recoverable."""
+
+        # set containing read_set recoverability violations, read/write pairs that are not recoverable.
+        violations = set()
         
         for item in self.read_set:
             read_op  = item.read_op
             write_op = item.write_op
 
-            tx_a = self.history.get_transaction_by_id(read_op.transaction_id)
-            tx_b = self.history.get_transaction_by_id(write_op.transaction_id)
+            read_tx = self.history.get_transaction_by_id(read_op.transaction_id)
+            write_tx = self.history.get_transaction_by_id(write_op.transaction_id)
 
-            is_recoverable = self.is_transaction_recoverable(tx_a, tx_b)
+            pair_recoverable = self.determine_tx_pair_recoverability(read_tx, write_tx)
 
             # Add unrecoverable node tuples to a set
-            if not is_recoverable:
-                self.recoverability_infractions.add(item)
+            if not pair_recoverable:
+                violations.add((read_tx, write_tx))
+        
+        return IsRecoverableResult(violations)
 
-    def display_recoverable_error_message(self, read_set_result):
+    def format_recoverable_error_message(self, read_set_result):
         read_op = read_set_result.read_op
         write_op = read_set_result.write_op
 
@@ -59,7 +150,7 @@ class RecoverableEngine:
         print('history is not recoverable because T{0} reads from T{1} and T{0} {2} before T{1} {3}.'
             .format(read_tx.transaction_id, write_tx.transaction_id, read_formatted_commit_type, write_formatted_commit_type))
 
-    def is_transaction_recoverable(self, tx_a, tx_b):
+    def determine_tx_pair_recoverability(self, tx_a, tx_b):
         """tx_a is dependent on / reads from tx_b"""
         a_order = self.tx_commit_abort_order.index(tx_a.transaction_id)
         b_order = self.tx_commit_abort_order.index(tx_b.transaction_id)
@@ -84,7 +175,7 @@ class RecoverableEngine:
         # we should never reach this case?
         raise Exception('what the hell!')
 
-    def determine_transaction_terminate_order(self):
+    def build_transaction_commit_abort_order(self):
         for item in self.history.schedule:
             if item.is_abort() or item.is_commit():
                 self.tx_commit_abort_order.append(item.transaction_id)
@@ -129,7 +220,7 @@ class RecoverableEngine:
             if write_found is None:
                 continue
             
-            start_idx = schedule.index(write_found) + 1
+            start_idx = schedule.index(write_found)
 
             if start_idx >= idx:
                 raise Exception('invalid index bounds')
@@ -139,16 +230,10 @@ class RecoverableEngine:
 
             # An abort exists, so read_op does not read from write_op
             if abort_exists:
-                print('found abort!!!')
                 continue
             
             # There is a read from relation. Add a tuple to the read set
             self.read_set.add(read_set_result(read_op, write_op))
-    
-    def pretty_print(self):
-        for item in self.recoverability_infractions:
-            self.display_recoverable_error_message(item)
-
 
                 
 
