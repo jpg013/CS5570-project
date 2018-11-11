5948b76d92d7221b96ba1940a76590d4231b35bd
Justin <jpg013@gmail.com>
2018-11-11 12:50:22 -0600

    Production web-build

--- app.py
+++ app.py
@@ -76,6 +76,6 @@ def get_graphs():
         
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port, threaded=True, use_reloader=True)
 
